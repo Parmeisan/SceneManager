fe85d42c2161c1c1322f250140f0c240b4281bd7
PaulGGJ <paulsmart525@gmail.com>
2026-01-30 22:26:57 -0600

    More stuff on the player character
    He can conjure a red rectangle and fall to his death.

--- player.gd
+++ player.gd
@@ -4,7 +4,17 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var facing = "RIGHT"
 
+func _unhandled_input(event):
+	if event.get_class() == "InputEventKey":
+		if event.keycode == 4194326 && event.pressed == true:
+			print("Control pressed")
+			if facing == "RIGHT":
+				$ThePunchZone.position.x = 43
+			else:
+
+				$ThePunchZone.position.x = -43
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,7 +29,13 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if(velocity.x < 0):
+			facing = "LEFT"
+		if(velocity.x > 0):
+			facing = "RIGHT"
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if global_position.y > 2000:
+		get_tree().quit()
 
 	move_and_slide()
